e8d9af7942df8217a47e68e6fefbf067ac935c44
dantegg <luroufan327@hotmail.com>
2022-04-20 08:18:19 +0800

    update

---
+++ sm_web/src/main/java/com/dantegg/sm/global/DispatcherServlet.java
@@ -0,0 +1,20 @@
+package com.dantegg.sm.global;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DispatcherServlet extends GenericServlet {
+
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+    }
+}
